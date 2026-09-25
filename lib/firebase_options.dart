b0fe0dfe0b25977629a3431ceab74b34ab246bc0
corevanta2001 <mundexpurex@gmail.com>
2026-09-25 05:15:39 +0000

    fix: ready for build

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -32,8 +32,8 @@ class DefaultFirebaseOptions {
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyAJR1-V0x-JdrMHwJIxYosHYvi77nFNn7c',
-    appId: '1:697104593083:android:06e996e87ae1d63883b99e',
+    apiKey: 'AIzaSyB26Oclj50Oy4spVQfHcbzAk16Bjri25Hc',
+    appId: '1:697104593083:android:dd64312e6ba30f3583b99e',
     messagingSenderId: '697104593083',
     projectId: 'forceapp-2573e',
     storageBucket: 'forceapp-2573e.firebasestorage.app',
